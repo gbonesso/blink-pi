19a40ea7d7abcb4bb0208404d192325b2a33df34
Gustavo Adolpho Bonesso <gustavo_bonesso@hotmail.com>
2024-04-30 12:23:09 -0300

    teste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+from picamera2.array import PiRGBArray
+from picamera2 import PiCamera
 import time
 import cv2
 # initialize the camera and grab a reference to the raw camera capture
