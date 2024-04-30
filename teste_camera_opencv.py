be4a9ac39970b0638e6b79c356c5b3b9a05c2970
Gustavo Adolpho Bonesso <gustavo_bonesso@hotmail.com>
2024-04-30 14:36:54 -0300

    teste

---
+++ teste_camera_opencv.py
@@ -0,0 +1,19 @@
+#https://stackoverflow.com/questions/71919403/problem-using-opencv-and-raspberry-pi-camera-v2
+
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+
+# set dimensions
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    cv2.imwrite('Atharva_Naik.jpg', frame)
+    cv2.imshow('Video', frame)
+    cv2.waitKey(0)
+
+cap.release()
+cv2.destroyAllWindows()
