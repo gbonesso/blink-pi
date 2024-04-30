1f3ccf0170b071be6e168fafb32b31f1e40f0def
Gustavo Adolpho Bonesso <gustavo_bonesso@hotmail.com>
2024-04-30 17:55:33 -0300

    teste

--- teste_PIL_image.py
+++ teste_PIL_image.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from picamera2 import Picamera2
 import time
 picam2 = Picamera2()
@@ -5,3 +6,7 @@ picam2.start()
 time.sleep(1)
 image = picam2.capture_image("main")
 print(image)
+im_array = picam2.capture_array("main")
+im = Image.fromarray(im_array)
+print(im)
+im.show()
