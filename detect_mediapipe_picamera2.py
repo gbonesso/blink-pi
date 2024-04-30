61191582b353dc8973c32168e1e2ee2330599d77
Gustavo Adolpho Bonesso <gustavo_bonesso@hotmail.com>
2024-04-30 18:50:06 -0300

    teste

--- detect_mediapipe_picamera2.py
+++ detect_mediapipe_picamera2.py
@@ -117,10 +117,10 @@ def run(model: str, num_faces: int,
         #image = Image.fromarray(im_array)
 
         image = cv2.cvtColor(im_array, cv2.COLOR_RGBA2BGR)
-        #print(image)
+        print(image)
 
-        cv2.imshow('face_landmarker', image)
-        key = cv2.waitKey(0)
+        #cv2.imshow('face_landmarker', image)
+        #key = cv2.waitKey(0)
 
         im = Image.fromarray(im_array)
         print('PIL Image:', im)
