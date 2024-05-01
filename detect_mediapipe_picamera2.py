e6cf549c89f28194dd77c50e4a69e51b2cdc21b3
Gustavo Adolpho Bonesso <gustavo_bonesso@hotmail.com>
2024-05-01 10:46:37 -0300

    teste

--- detect_mediapipe_picamera2.py
+++ detect_mediapipe_picamera2.py
@@ -44,15 +44,15 @@ COUNTER, FPS = 0, 0
 START_TIME = time.time()
 DETECTION_RESULT = None
 
-ear_left_label = None
+#ear_left_label = None
 
 class LoginScreen(GridLayout):
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-        global ear_left_label
+        #global ear_left_label
         self.cols = 2
-        ear_left_label = Label(text='EAR L:')
+        self.ear_left_label = Label(text='EAR L:')
         self.add_widget(ear_left_label)
         self.ear_right_label = Label(text='EAR R:')
         self.add_widget(self.ear_right_label)
@@ -65,189 +65,190 @@ class MyApp(App):
         #main()
         return self.login_screen
 
-def run(model: str, num_faces: int,
-        min_face_detection_confidence: float,
-        min_face_presence_confidence: float, min_tracking_confidence: float,
-        camera_id: int, width: int, height: int) -> None:
-    """Continuously run inference on images acquired from the camera.
+    def run(self, model: str, num_faces: int,
+            min_face_detection_confidence: float,
+            min_face_presence_confidence: float, min_tracking_confidence: float,
+            camera_id: int, width: int, height: int) -> None:
+        """Continuously run inference on images acquired from the camera.
 
-  Args:
-      model: Name of the face landmarker model bundle.
-      num_faces: Max number of faces that can be detected by the landmarker.
-      min_face_detection_confidence: The minimum confidence score for face
-        detection to be considered successful.
-      min_face_presence_confidence: The minimum confidence score of face
-        presence score in the face landmark detection.
-      min_tracking_confidence: The minimum confidence score for the face
-        tracking to be considered successful.
-      camera_id: The camera id to be passed to OpenCV.
-      width: The width of the frame captured from the camera.
-      height: The height of the frame captured from the camera.
-  """
+      Args:
+          model: Name of the face landmarker model bundle.
+          num_faces: Max number of faces that can be detected by the landmarker.
+          min_face_detection_confidence: The minimum confidence score for face
+            detection to be considered successful.
+          min_face_presence_confidence: The minimum confidence score of face
+            presence score in the face landmark detection.
+          min_tracking_confidence: The minimum confidence score for the face
+            tracking to be considered successful.
+          camera_id: The camera id to be passed to OpenCV.
+          width: The width of the frame captured from the camera.
+          height: The height of the frame captured from the camera.
+      """
 
-    global ear_left_label
+        global ear_left_label
 
-    # Start capturing video input from the camera
-    '''cap = cv2.VideoCapture(camera_id)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)'''
+        # Start capturing video input from the camera
+        '''cap = cv2.VideoCapture(camera_id)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)'''
 
-    picam2 = Picamera2()
-    picam2.start()
+        picam2 = Picamera2()
+        picam2.start()
 
-    # Visualization parameters
-    row_size = 50  # pixels
-    left_margin = 24  # pixels
-    text_color = (0, 0, 0)  # black
-    font_size = 1
-    font_thickness = 1
-    fps_avg_frame_count = 10
+        # Visualization parameters
+        row_size = 50  # pixels
+        left_margin = 24  # pixels
+        text_color = (0, 0, 0)  # black
+        font_size = 1
+        font_thickness = 1
+        fps_avg_frame_count = 10
 
-    # Label box parameters
-    label_background_color = (255, 255, 255)  # White
-    label_padding_width = 1500  # pixels
+        # Label box parameters
+        label_background_color = (255, 255, 255)  # White
+        label_padding_width = 1500  # pixels
 
-    def save_result(result: vision.FaceLandmarkerResult,
-                    unused_output_image: mp.Image, timestamp_ms: int):
-        global FPS, COUNTER, START_TIME, DETECTION_RESULT
+        def save_result(result: vision.FaceLandmarkerResult,
+                        unused_output_image: mp.Image, timestamp_ms: int):
+            global FPS, COUNTER, START_TIME, DETECTION_RESULT
 
-        # Calculate the FPS
-        if COUNTER % fps_avg_frame_count == 0:
-            FPS = fps_avg_frame_count / (time.time() - START_TIME)
-            START_TIME = time.time()
+            # Calculate the FPS
+            if COUNTER % fps_avg_frame_count == 0:
+                FPS = fps_avg_frame_count / (time.time() - START_TIME)
+                START_TIME = time.time()
 
-        DETECTION_RESULT = result
-        COUNTER += 1
+            DETECTION_RESULT = result
+            COUNTER += 1
 
-    # Initialize the face landmarker model
-    base_options = python.BaseOptions(model_asset_path=model)
-    options = vision.FaceLandmarkerOptions(
-        base_options=base_options,
-        running_mode=vision.RunningMode.LIVE_STREAM,
-        num_faces=num_faces,
-        min_face_detection_confidence=min_face_detection_confidence,
-        min_face_presence_confidence=min_face_presence_confidence,
-        min_tracking_confidence=min_tracking_confidence,
-        output_face_blendshapes=True,
-        result_callback=save_result)
-    detector = vision.FaceLandmarker.create_from_options(options)
+        # Initialize the face landmarker model
+        base_options = python.BaseOptions(model_asset_path=model)
+        options = vision.FaceLandmarkerOptions(
+            base_options=base_options,
+            running_mode=vision.RunningMode.LIVE_STREAM,
+            num_faces=num_faces,
+            min_face_detection_confidence=min_face_detection_confidence,
+            min_face_presence_confidence=min_face_presence_confidence,
+            min_tracking_confidence=min_tracking_confidence,
+            output_face_blendshapes=True,
+            result_callback=save_result)
+        detector = vision.FaceLandmarker.create_from_options(options)
 
-    # Continuously capture images from the camera and run inference
-    #while cap.isOpened():
-    while True:
-        '''success, image = cap.read()
-        if not success:
-            sys.exit(
-                'ERROR: Unable to read from webcam. Please verify your webcam settings.'
-            )'''
+        # Continuously capture images from the camera and run inference
+        #while cap.isOpened():
+        while True:
+            '''success, image = cap.read()
+            if not success:
+                sys.exit(
+                    'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+                )'''
 
-        im_array = picam2.capture_array("main")
-        #image = Image.fromarray(im_array)
+            im_array = picam2.capture_array("main")
+            #image = Image.fromarray(im_array)
 
-        image = cv2.cvtColor(im_array, cv2.COLOR_RGBA2BGR)
-        #print(image)
+            image = cv2.cvtColor(im_array, cv2.COLOR_RGBA2BGR)
+            #print(image)
 
-        #cv2.imshow('face_landmarker', image)
-        #key = cv2.waitKey(0)
+            #cv2.imshow('face_landmarker', image)
+            #key = cv2.waitKey(0)
 
-        im = Image.fromarray(im_array)
-        print('PIL Image:', im)
+            im = Image.fromarray(im_array)
+            print('PIL Image:', im)
 
-        image = cv2.flip(image, 1)
+            image = cv2.flip(image, 1)
 
-        # Convert the image from BGR to RGB as required by the TFLite model.
-        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image)
+            # Convert the image from BGR to RGB as required by the TFLite model.
+            rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image)
 
-        # Run face landmarker using the model.
-        detector.detect_async(mp_image, time.time_ns() // 1_000_000)
+            # Run face landmarker using the model.
+            detector.detect_async(mp_image, time.time_ns() // 1_000_000)
 
-        # Show the FPS
-        fps_text = 'FPS = {:.1f}'.format(FPS)
-        print(fps_text)
-        '''text_location = (left_margin, row_size)
-        current_frame = image
-        cv2.putText(current_frame, fps_text, text_location,
-                    cv2.FONT_HERSHEY_DUPLEX,
-                    font_size, text_color, font_thickness, cv2.LINE_AA)'''
+            # Show the FPS
+            fps_text = 'FPS = {:.1f}'.format(FPS)
+            print(fps_text)
+            '''text_location = (left_margin, row_size)
+            current_frame = image
+            cv2.putText(current_frame, fps_text, text_location,
+                        cv2.FONT_HERSHEY_DUPLEX,
+                        font_size, text_color, font_thickness, cv2.LINE_AA)'''
 
-        # DETECTION_RESULT é setado no callback...
-        #print(DETECTION_RESULT)
+            # DETECTION_RESULT é setado no callback...
+            #print(DETECTION_RESULT)
 
-        if DETECTION_RESULT is not None:
-            print('EAR:', get_ear_values(DETECTION_RESULT))
-            print(ear_left_label)
-            if ear_left_label is not None:
-                ear_left_label.text = get_ear_values(DETECTION_RESULT)
-
-
-        #cv2.imshow('face_landmarker', current_frame)
-
-        # Stop the program if the ESC key is pressed.
-        if cv2.waitKey(1) == 27:
-            break
-
-    detector.close()
-    #cap.release()
-    #cv2.destroyAllWindows()
+            if DETECTION_RESULT is not None:
+                print('EAR:', get_ear_values(DETECTION_RESULT))
+                print(ear_left_label)
+                if ear_left_label is not None:
+                    ear_left_label.text = get_ear_values(DETECTION_RESULT)
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(
-        '--model',
-        help='Name of face landmarker model.',
-        required=False,
-        default='face_landmarker.task')
-    parser.add_argument(
-        '--numFaces',
-        help='Max number of faces that can be detected by the landmarker.',
-        required=False,
-        default=1)
-    parser.add_argument(
-        '--minFaceDetectionConfidence',
-        help='The minimum confidence score for face detection to be considered '
-             'successful.',
-        required=False,
-        default=0.5)
-    parser.add_argument(
-        '--minFacePresenceConfidence',
-        help='The minimum confidence score of face presence score in the face '
-             'landmark detection.',
-        required=False,
-        default=0.5)
-    parser.add_argument(
-        '--minTrackingConfidence',
-        help='The minimum confidence score for the face tracking to be '
-             'considered successful.',
-        required=False,
-        default=0.5)
-    # Finding the camera ID can be very reliant on platform-dependent methods.
-    # One common approach is to use the fact that camera IDs are usually indexed sequentially by the OS, starting from 0.
-    # Here, we use OpenCV and create a VideoCapture object for each potential ID with 'cap = cv2.VideoCapture(i)'.
-    # If 'cap' is None or not 'cap.isOpened()', it indicates the camera ID is not available.
-    parser.add_argument(
-        '--cameraId', help='Id of camera.', required=False, default=0)
-    parser.add_argument(
-        '--frameWidth',
-        help='Width of frame to capture from camera.',
-        required=False,
-        default=1280)
-    parser.add_argument(
-        '--frameHeight',
-        help='Height of frame to capture from camera.',
-        required=False,
-        default=960)
-    args = parser.parse_args()
+            #cv2.imshow('face_landmarker', current_frame)
 
-    run(args.model, int(args.numFaces), args.minFaceDetectionConfidence,
-        args.minFacePresenceConfidence, args.minTrackingConfidence,
-        int(args.cameraId), args.frameWidth, args.frameHeight)
+            # Stop the program if the ESC key is pressed.
+            if cv2.waitKey(1) == 27:
+                break
+
+        detector.close()
+        #cap.release()
+        #cv2.destroyAllWindows()
+
+
+    def main(self):
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.add_argument(
+            '--model',
+            help='Name of face landmarker model.',
+            required=False,
+            default='face_landmarker.task')
+        parser.add_argument(
+            '--numFaces',
+            help='Max number of faces that can be detected by the landmarker.',
+            required=False,
+            default=1)
+        parser.add_argument(
+            '--minFaceDetectionConfidence',
+            help='The minimum confidence score for face detection to be considered '
+                 'successful.',
+            required=False,
+            default=0.5)
+        parser.add_argument(
+            '--minFacePresenceConfidence',
+            help='The minimum confidence score of face presence score in the face '
+                 'landmark detection.',
+            required=False,
+            default=0.5)
+        parser.add_argument(
+            '--minTrackingConfidence',
+            help='The minimum confidence score for the face tracking to be '
+                 'considered successful.',
+            required=False,
+            default=0.5)
+        # Finding the camera ID can be very reliant on platform-dependent methods.
+        # One common approach is to use the fact that camera IDs are usually indexed sequentially by the OS, starting from 0.
+        # Here, we use OpenCV and create a VideoCapture object for each potential ID with 'cap = cv2.VideoCapture(i)'.
+        # If 'cap' is None or not 'cap.isOpened()', it indicates the camera ID is not available.
+        parser.add_argument(
+            '--cameraId', help='Id of camera.', required=False, default=0)
+        parser.add_argument(
+            '--frameWidth',
+            help='Width of frame to capture from camera.',
+            required=False,
+            default=1280)
+        parser.add_argument(
+            '--frameHeight',
+            help='Height of frame to capture from camera.',
+            required=False,
+            default=960)
+        args = parser.parse_args()
+
+        self.run(args.model, int(args.numFaces), args.minFaceDetectionConfidence,
+            args.minFacePresenceConfidence, args.minTrackingConfidence,
+            int(args.cameraId), args.frameWidth, args.frameHeight)
 
 
 if __name__ == '__main__':
     #main()
-    p1 = Process(target=main)
-    p1.start()
-    MyApp().run()
+    #p1 = Process(target=main)
+    #p1.start()
+    MyApp().main()
+
