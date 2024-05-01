84418a292862a04c3da9f7f822d87cac74bb7a04
Gustavo Adolpho Bonesso <gustavo_bonesso@hotmail.com>
2024-05-01 10:53:53 -0300

    teste

--- detect_mediapipe_picamera2.py
+++ detect_mediapipe_picamera2.py
@@ -181,7 +181,7 @@ class MyApp(App):
                 print('EAR:', get_ear_values(DETECTION_RESULT))
                 print(self.login_screen.ear_left_label)
                 if self.login_screen.ear_left_label is not None:
-                    self.login_screen.ear_left_label.text = get_ear_values(DETECTION_RESULT)
+                    self.login_screen.ear_left_label.text = str(get_ear_values(DETECTION_RESULT)[0])
 
 
             #cv2.imshow('face_landmarker', current_frame)
